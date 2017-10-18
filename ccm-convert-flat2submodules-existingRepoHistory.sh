19a65b8f7895b0c2246505c0042e22fb3c2b458b
Claus Schneider(Praqma) <claus.schneider-ext@man.eu>
2017-10-18 11:59:23 +0200

    SCARP-154 script error and remove odd_baselines

--- ccm-convert-flat2submodules-existingRepoHistory.sh
+++ ccm-convert-flat2submodules-existingRepoHistory.sh
@@ -21,7 +21,7 @@ if [ ! -e ${repo_name} ] ; then
     git clone --recursive ${git_remote_repo}
     cd ${repo_name}
     git checkout -B master ${repo_init_tag}
-    git reset --hard ${repo_init_tag}
+    git reset --hard ${repo_init_tag} >> /dev/null
     git clean -xffd
 
     export GIT_AUTHOR_DATE=$(git tag -l --format="%(taggerdate:iso8601)" ${repo_init_tag} | awk -F" " '{print $1 " " $2}')
@@ -36,22 +36,24 @@ if [ ! -e ${repo_name} ] ; then
 
     git commit -C "$repo_init_tag" --amend --reset-author
     git tag -a -m $(git tag -l --format '%(contents)' ${repo_init_tag}) ${repo_name}/${repo_init_tag}/${repo_init_tag}
-    git tag -a -m $(git tag -l --format '%(contents)' ${repo_init_tag}) ${repo_name}/odd_baselines/odd_baselines
 
     unset GIT_AUTHOR_DATE
     unset GIT_COMMITTER_DATE
 
-    git reset --hard ${repo_name}/${repo_init_tag}/${repo_init_tag}
+    git reset --hard ${repo_name}/${repo_init_tag}/${repo_init_tag} >> /dev/null
     git clean -xffd
     pwd
     # we are still in the root repo
 else
-    echo "Already cloned and initialized - skip "
-    git fetch -ap
+    echo "Already cloned and initialized"
+    echo "Reset all tags to remote"
+    grep "${repo_name}/.*/.*_[dprtis][eueenq][lblsta]$" |  xargs git tag --delete
+    git fetch --tags
     pwd
     cd ${repo_name}
 fi
 
+set +x
 export project_revisions=$(for tag in $(git log --topo-order --oneline --all --decorate \
                                     | awk -F"(" '{print $2}' \
                                     | awk -F")" '{print $1}' \
@@ -66,18 +68,18 @@ export project_revisions=$(for tag in $(git log --topo-order --oneline --all --d
                             | grep -v master \
                             | grep -v ${repo_name}/${repo_init_tag}$ \
                             | grep -v ${repo_name}/${repo_init_tag}/${repo_init_tag}$ \
-                            | grep -v ${repo_name}/odd_baselines/odd_baselines$ \
                             | grep -v ${repo_name}/.*/.*_[dprtis][eueenq][lblsta]$ \
                             | grep -v ${repo_init_tag}$ \
                             | tac \
                            )
-
+set -x
+echo "${project_revisions}"
 for project_revision in ${project_revisions}; do
     repo_convert_rev_tag=${project_revision}
+    ccm_repo_convert_rev_tag=${repo_convert_rev_tag:: -4}
 
-    #repo_convert_rev_tag=`echo ${project_revision} | awk -F"@@@" '{print $1}' | awk -F"~" '{print $2}'`
-    ccm_baseline_obj_this=$(ccm query "has_project_in_baseline('${repo_name}~$(echo ${repo_convert_rev_tag:: -4} | sed -e 's/xxx/ /g'):project:1') and release='$(ccm query "name='${repo_name}' and version='$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g')' and type='project'" -u -f "%release")'" -u -f "%objectname" | head -1 )
-    ccm_component_release=`ccm attr -show release "${repo_name}~$(echo ${repo_convert_rev_tag:: -4} | sed -e 's/xxx/ /g'):project:1" | sed -e 's/ //g'`
+    ccm_baseline_obj_this=$(ccm query "has_project_in_baseline('${repo_name}~$(echo ${ccm_repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:1') and release='$(ccm query "name='${repo_name}' and version='$(echo ${ccm_repo_convert_rev_tag} | sed -e 's/xxx/ /g')' and type='project'" -u -f "%release")'" -u -f "%objectname" | head -1 )
+    ccm_component_release=`ccm attr -show release "${repo_name}~$(echo ${ccm_repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:1" | sed -e 's/ //g'`
     ccm_release=$(echo ${ccm_component_release} | cut -d "/" -f 2)
 
     test "${ccm_release}x" == "x" && exit 1
@@ -91,34 +93,35 @@ for project_revision in ${project_revisions}; do
     # Get the right content
     if [ `git describe ${repo_convert_rev_tag}`  ] ; then
         # we do have the correct 'content' tag checkout it out
-        git reset --hard ${repo_convert_rev_tag}
+        git reset --hard ${repo_convert_rev_tag} >> /dev/null
     else
         # we do not have the 'content' tag available - investigate its history if it exists ( e.g. missing in repo )
-        ./ccm-baseline-history-get-root.sh "${repo_name}~$(echo ${repo_convert_rev_tag:: -4} | sed -e 's/xxx/ /g')"
+        ./ccm-baseline-history-get-root.sh "${repo_name}~$(echo ${ccm_repo_convert_rev_tag} | sed -e 's/xxx/ /g')"
         exit 1
     fi
 
-    git clean -xffd
+    git clean -xffd >> /dev/null
 
     baseline_from_tag_info=$(git show ${repo_convert_rev_tag} | grep "1) ${repo_name}~" | awk -F"~" '{print $2}')
     if [ "${baseline_from_tag_info}X" != "X" ] ; then
         repo_baseline_rev_tag_wcomponent_wstatus=$(git tag | grep "${repo_name}/.*/${baseline_from_tag_info}_[dprtis][eueenq][lblsta]$" || grep_ext_value=$? )
         if [ "${repo_baseline_rev_tag_wcomponent_wstatus}x" == "x" ] ; then
-            repo_baseline_rev_tag_wcomponent_wstatus="${repo_name}/odd_baselines/odd_baselines"
+            exit 1
         fi
     else
         repo_baseline_rev_tag_wcomponent_wstatus="${repo_name}/${repo_init_tag}/${repo_init_tag}"
     fi
 
     # Move the workarea pointer to the 'baseline' tag
-    git reset --mixed ${repo_baseline_rev_tag_wcomponent_wstatus}
+    git reset --mixed ${repo_baseline_rev_tag_wcomponent_wstatus} >> /dev/null
     git checkout HEAD .gitignore
-    git status
 
     for repo_submodule in ${repo_submodules}; do
-        repo_submodule_rev=$(ccm query "hierarchy_project_members('${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:1',none ) and name='${sub_proj_n
-ame}'" -u -f "%version" | sed -s 's/ /xxx/g')
-        #repo_submodule_rev=$(git show ${repo_convert_rev_tag} | grep "    ${repo_submodule}~" | awk -F"~" '{print $2}' | awk -F" " '{print $1}')
+        repo_submodule_rev=$(ccm query " \
+                                   hierarchy_project_members(\
+                                       '${ccm_project_name}~$(echo ${ccm_repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:1',none \
+                                   ) \
+                                   and name='${repo_submodule}'" -u -f "%version" | sed -s 's/ /xxx/g')
         if [ "${repo_submodule_rev}X" == "X" ] ; then
             echo "The submodule does not exit as a project - skip"
             continue
@@ -137,7 +140,7 @@ ame}'" -u -f "%version" | sed -s 's/ /xxx/g')
 
         if [ `git describe ${repo_convert_rev_tag_wcomponent_wstatus}` ] ; then
             # we already have the correct tag, so just set it and move on..
-            git checkout ${repo_submodule_rev}
+            git checkout ${repo_convert_rev_tag_wcomponent_wstatus}
             git clean -xffd
             repo_submodule_rev=""
             cd -
@@ -158,7 +161,8 @@ ame}'" -u -f "%version" | sed -s 's/ /xxx/g')
             exit 1
         fi
 
-        git tag -f -a -m "Please see tag in master repo for info: ${repo_convert_rev_tag_wcomponent_wstatus}" ${repo_convert_rev_tag_wcomponent_wstatus}
+        git tag -f -a -m "Please see tag in master repo for info: ${repo_convert_rev_tag_wcomponent_wstatus}" \
+                            ${repo_convert_rev_tag_wcomponent_wstatus}
 
         git push origin -f --tag ${repo_convert_rev_tag_wcomponent_wstatus}
 
@@ -167,14 +171,12 @@ ame}'" -u -f "%version" | sed -s 's/ /xxx/g')
         cd -
 
     done
-    git status
-    git add -A .
-    #git status
+    git add -A . >> /dev/null
 
     export GIT_AUTHOR_DATE=$(git tag -l --format="%(taggerdate:iso8601)" ${repo_convert_rev_tag} | awk -F" " '{print $1 " " $2}')
     export GIT_COMMITTER_DATE=${GIT_AUTHOR_DATE}
 
-    git commit -C ${repo_convert_rev_tag} --reset-author || ( echo "Empty commit.." )
+    git commit -C ${repo_convert_rev_tag} --reset-author >> /dev/null || ( echo "Empty commit.." )
 
     git tag -l --format '%(contents)' ${repo_convert_rev_tag} > ./tag_meta_data.txt
     git tag -a -F ./tag_meta_data.txt ${repo_convert_rev_tag_wcomponent_wstatus}
