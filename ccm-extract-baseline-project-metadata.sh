0d5d7fcb5587365a6b4ed25983c5eafc3995623b
Claus Schneider(Praqma) <claus.schneider-ext@man.eu>
2019-02-12 11:12:29 +0100

    SCAUG-57 Metadata extraction: setup attribute and relations based on database name of the running CCM session

--- ccm-extract-baseline-project-metadata.sh
+++ ccm-extract-baseline-project-metadata.sh
@@ -5,12 +5,35 @@ ccm_project_name=$1
 repo_convert_rev_tag=$2
 repo_convert_instance=$3
 
+ccm_current_db=`ccm status -f "%database %current_session" | grep TRUE | awk -F " " '{print $1}'`
+case ${ccm_current_db} in
+    /data/ccmdb/db_functionDevelopment|/data/ccmdb/db_automation|/data/ccmdb/db_module|/data/ccmdb/db_application)
+        epic_level_header="Change Requests: (CR)"
+        epic_level_release_attr="TargetRelease"
+        epic_level_epic2story_relation="associatedWP"
+
+        story_level_header="Work Packages (WP)"
+        story_level_release_attr="TargetRelease"
+        require_baseline_object="true"
+        ;;
+    /data/ccmdb/ME_ECS)
+        epic_level_header="Master Change Requests: (MCR)"
+        epic_level_release_attr="release"
+        epic_level_epic2story_relation="associatedImpl"
+
+        story_level_header="Implementation Change Requests(ICR)"
+        story_level_release_attr="release"
+        ;;
+    *)
+        echo "Undetermined/supported: ccm_current_db: ${ccm_current_db}"
+        exit 1
+esac
+
 test "${ccm_project_name}x" == "x"      && ( echo "'ccm_project_name' not set - exit"       && exit 1 )
 test "${repo_convert_rev_tag}x" == "x"  && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
 test "${repo_convert_instance}x" == "x"  && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
 
 ccm_baseline_obj_this=$(ccm query "has_project_in_baseline('${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}') and release='$(ccm query "name='${ccm_project_name}' and version='$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g')' and type='project'" -u -f "%release")'" -u -f "%objectname" | head -1 )
-
 if [ "${ccm_baseline_obj_this}X" != "X" ]; then
     echo > ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
@@ -28,30 +51,30 @@ if [ "${ccm_baseline_obj_this}X" != "X" ]; then
 
     echo >> ./tag_meta_data.txt
     echo "---------------------------------------------------------">> ./tag_meta_data.txt
-    echo "Master Change Requests (MCR):"                            >> ./tag_meta_data.txt
+    echo "${epic_level_header}:"                                    >> ./tag_meta_data.txt
     echo "---------------------------------------------------------">> ./tag_meta_data.txt
-    ccm query "has_associatedImpl(has_associated_task(is_dirty_task_in_baseline_of('${ccm_baseline_obj_this}')))" -u -f "%displayname %resolver %release %problem_synopsis" >> ./tag_meta_data.txt || echo "<none>" >> ./tag_meta_data.txt
+    ccm query "has_${epic_level_epic2story_relation}(has_associated_task((is_task_in_baseline_of('${ccm_baseline_obj_this}') or is_dirty_task_in_baseline_of('${ccm_baseline_obj_this}'))))" -f "%displayname %resolver %${epic_level_release_attr} %problem_synopsis" >> ./tag_meta_data.txt || echo "<none>" >> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
 
     echo >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    echo "Fully integrated Implementation Change Requests(ICR):"     >> ./tag_meta_data.txt
+    echo "Fully integrated ${story_level_header}:"                   >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    ccm baseline -show fully_included_change_requests -groupby "Release: %release"  -f "%displayname %resolver %release %problem_synopsis" "${ccm_baseline_obj_this}" >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
+    ccm baseline -show fully_included_change_requests -groupby "${epic_level_release_attr}: %${epic_level_release_attr}"  -f "%displayname %resolver %${epic_level_release_attr} %problem_synopsis" "${ccm_baseline_obj_this}" >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
 
     echo >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    echo "Partially integrated Implementation Change Requests(ICR):" >> ./tag_meta_data.txt
+    echo "Partially integrated ${story_level_header}:"               >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    ccm baseline -show fully_included_change_requests -groupby "Release: %release" -f "%displayname %resolver %release %problem_synopsis" "${ccm_baseline_obj_this}" >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
+    ccm baseline -show partially_included_change_requests -groupby "${epic_level_release_attr}: %${epic_level_release_attr}" -f "%displayname %resolver %${epic_level_release_attr} %problem_synopsis" "${ccm_baseline_obj_this}" >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
 
     echo >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
     echo "Tasks integrated in baseline:"                             >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    ccm baseline -show tasks -f "%displayname %{create_time[dateformat='yyyy-M-dd HH:MM:SS']} %resolver %status %release %task_synopsis" "${ccm_baseline_obj_this}" >> ./tag_meta_data.txt || echo "<none>" >> ./tag_meta_data.txt
+    ccm baseline -show tasks -f "%displayname %{create_time[dateformat='yyyy-MM-dd HH:MM:SS']} %resolver %status %release %task_synopsis" "${ccm_baseline_obj_this}" >> ./tag_meta_data.txt || echo "<none>" >> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
 
     echo >> ./tag_meta_data.txt
@@ -62,9 +85,10 @@ if [ "${ccm_baseline_obj_this}X" != "X" ]; then
     echo >> ./tag_meta_data.txt
 
 else
+    [[ "${require_baseline_object}" == "true" ]] && ( echo "ERROR: It is expected to have a baseline object due to configuration: require_baseline_object=true for this database: ${ccm_current_db}" && exit 2 )
     echo > ./tag_meta_data.txt
     echo "---------------------------------------------------------">> ./tag_meta_data.txt
-    echo "NO BASELINE OBJECT ASSOCIATED WITH THIS PROJECT REVISION" >> ./tag_meta_data.txt
+    echo "NO BASELINE OBJECT ASSOCIATED WITH THIS PROJECT VERSION"  >> ./tag_meta_data.txt
     echo "---------------------------------------------------------">> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
 
@@ -77,13 +101,13 @@ else
 
     echo >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    echo "Master Change Requests (MCR):"                             >> ./tag_meta_data.txt
+    echo "${epic_level_header}:"                                     >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    ccm query "has_associatedImpl(has_associated_task(is_task_in_folder_of(is_folder_in_rp_of('${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}'))))" -f "%displayname %resolver %release %problem_synopsis" >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
+    ccm query "has_${epic_level_epic2story_relation}(has_associated_task(is_task_in_folder_of(is_folder_in_rp_of('${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}'))))" -f "%displayname %resolver %release %problem_synopsis" >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
 
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
-    echo "Related/Integrated Implementation Change Requests(ICR):"   >> ./tag_meta_data.txt
+    echo "Related/Integrated ${story_level_header}:"   >> ./tag_meta_data.txt
     echo "---------------------------------------------------------" >> ./tag_meta_data.txt
     ccm query "has_associated_task(is_task_in_folder_of(is_folder_in_rp_of('${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}')))" -f "%displayname %resolver %release %problem_synopsis"  >> ./tag_meta_data.txt  || echo "<none>" >> ./tag_meta_data.txt
     echo >> ./tag_meta_data.txt
