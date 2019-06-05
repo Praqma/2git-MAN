e9f0999264fc4e6a1f74f749e677cd5b79bde4a8
Claus Schneider(Praqma) <claus.schneider-ext@man.eu>
2019-06-05 14:39:36 +0200

    SCARP-356 Skip baseline if already in the projects files for conversion

--- ccm-baseline-history.sh
+++ ccm-baseline-history.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-#set -x #
+[[ $debug == "TRUE" ]] && ( set -x && debug="true" )
 set -e
-
-#export PATH="/c/Program Files (x86)/IBM/Rational/Synergy/7.2.1/bin:${PATH}"
-#export PATH="/mnt/synergy/ccm721/bin/ccm:${PATH}"
 
 BASELINE_PROJECT=$1
 UNTIL_PROJECT=$2
@@ -18,11 +15,14 @@ handle_baseline2(){
     # All status versions
     query="has_baseline_project(name match '${proj_name}*' and version='${proj_version}' and type='project' and instance='${proj_instance}') and ( status='integrate' or status='test' or status='sqa' or status='released' )"
     local SUCCESSOR_PROJECTS=`ccm query "${query}" -u -f "%objectname" | sed -e 's/ /xxx/g'`
+    [[ $debug == "true" ]] && printf "_________________________\n$SUCCESSOR_PROJECTS\n"
     for SUCCESSOR_PROJECT in ${SUCCESSOR_PROJECTS} ; do
-		local inherited_string="${inherited_string_local} -> \"${BASELINE_PROJECT}\""
-		printf "$SUCCESSOR_PROJECT@@@$CURRENT_PROJECT\n" >> ${projects_file}
-		handle_baseline2 ${SUCCESSOR_PROJECT} "${inherited_string}"
-	done
+        local inherited_string="${inherited_string_local} -> ${CURRENT_PROJECT}"
+        [[ $debug == "true" ]] && printf "${inherited_string}\n"
+        grep "$SUCCESSOR_PROJECT@@@$CURRENT_PROJECT" ${projects_file} && continue # Next if already for some odd reason exists - seen in firebird~BES-SW-0906-1.8:project:2
+        printf "$SUCCESSOR_PROJECT@@@$CURRENT_PROJECT\n" >> ${projects_file}
+        handle_baseline2 ${SUCCESSOR_PROJECT} "${inherited_string}"
+    done
 }
 
 init_project_name=`printf "${BASELINE_PROJECT}" | awk -F"~" '{print $1}'`
@@ -37,9 +37,10 @@ if [ "${use_cached_project_list}X" == "trueX" ]; then
   fi
 fi
 
-inherited_string="\"${BASELINE_PROJECT}\""
+inherited_string="${BASELINE_PROJECT}"
 echo "$BASELINE_PROJECT@@@${init_project_name}~init:project:${instance}" > ${projects_file}
 
 handle_baseline2 ${BASELINE_PROJECT} ${inherited_string}
-cat ${projects_file}
-#rm -f ${projects_file}
+if [[ $debug != "true" ]]; then
+    cat ${projects_file}
+fi
