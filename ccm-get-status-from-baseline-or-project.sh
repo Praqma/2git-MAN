4ae03390968a7dbb337bf88189f241545b6cae37
Claus Schneider(Praqma) <claus.schneider-ext@man.eu>
2019-09-16 12:13:32 +0200

    Update status retrieved for first conversion ccm2git

--- ccm-get-status-from-baseline-or-project.sh
+++ ccm-get-status-from-baseline-or-project.sh
@@ -1,21 +1,34 @@
 #!/usr/bin/env bash
+set -u
+set -e
+
+# Load functions
+source ${BASH_SOURCE%/*}/_ccm-functions.sh || source ./_ccm-functions.sh
 
 ccm_project_name=$1
 repo_convert_rev_tag=$2
 repo_convert_instance=$3
 
-test "${ccm_project_name}x" == "x"      && ( echo "'ccm_project_name' not set - exit"       && exit 1 )
-test "${repo_convert_rev_tag}x" == "x"  && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
-test "${repo_convert_instance}x" == "x"  && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
+[[ "${ccm_project_name:-}" == "" ]]       && ( echo "'ccm_project_name' not set - exit"       && exit 1 )
+[[ "${repo_convert_rev_tag:-}" == "" ]]   && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
+[[ "${repo_convert_instance:-}" == "x" ]] && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
 
-ccm_baseline_obj_this=$(ccm query "has_project_in_baseline('${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}') and release='$(ccm query "name='${ccm_project_name}' and version='$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g')' and type='project' and instance='${repo_convert_instance}'" -u -f "%release")'" -u -f "%objectname" | head -1 )
-if [ "${ccm_baseline_obj_this}X" != "X" ]; then
-    ccm_baseline_status_this=`ccm attr -show status "${ccm_baseline_obj_this}" |  cut -c1-3 `
-else
-    ccm_baseline_status_this=`ccm attr -show status "${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}" |  sed -e 's/ //g' |  cut -c1-3`
+exit_code="0"
+find_n_set_baseline_obj_attrs_from_project "${ccm_project_name}~${repo_convert_rev_tag}:project:${repo_convert_instance}" "verbose_false" || exit_code=$?
+if [[ "${exit_code}" != "0" ]] ; then
+    echo "ERROR: Project not found: ${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}"
+    exit ${exit_code}
 fi
-if [ "${ccm_baseline_status_this}x" == "x" ] ; then
+
+if [[ "${ccm_baseline_status:-}" == "" ]]; then
+    # We could not set status from baseline object - take it from the project
+    ccm_baseline_status=`ccm attr -show status "${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}" |  sed -e 's/ //g' |  cut -c1-3`
+else
+    ccm_baseline_status=`echo ${ccm_baseline_status} |  cut -c1-3`
+fi
+if [[ "${ccm_baseline_status:-}" == "" ]] ; then
+    echo "Something went wrong as no status is set"
     exit 1
 else
-    echo ${ccm_baseline_status_this}
+    echo ${ccm_baseline_status}
 fi
