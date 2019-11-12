e599cf4d4991d410de8e4673b9ae428f7df6826d
Claus Schneider  (Praqma) <cls@praqma.net>
2019-11-12 13:38:41 +0100

    SCAUG-191 -> SCAUG-194 Add additional root finders - handles cross project name relation in baseline references

--- ccm-baseline-history-get-root.sh
+++ ccm-baseline-history-get-root.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
-#set -x
+[[ ${debug:-} == true ]] && set -x
+set -u
+set -e
 
-BASELINE_PROJECT=$1
-UNTIL_PROJECT=$2
+BASELINE_PROJECT="$1"
 
-until [ "${BASELINE_PROJECT}X" == "X" -o "$BASELINE_PROJECT" == "$UNTIL_PROJECT" ] ; do
-    BASELINE_PROJECT=`printf "${BASELINE_PROJECT}" | sed -e 's/xxx/ /g' `
+until [[ "${BASELINE_PROJECT:-}" == "" ]] ; do
+	this_project4part="${BASELINE_PROJECT}"
+	this_project_name=$(echo ${BASELINE_PROJECT} |  awk -F"~" '{print $1}')
 	query="is_baseline_project_of('${BASELINE_PROJECT}')"
-	BASELINE_PROJECT=`ccm query "is_baseline_project_of('${BASELINE_PROJECT}')" -u -f "%objectname" | sed -e 's/ /xxx/g'`
-	bl_print=`printf "${BASELINE_PROJECT}" | awk -F"~" '{print $2}'`
-	if [ "${BASELINE_PROJECT}X" != "X" ] ; then
-		printf "${BASELINE_PROJECT} -> "
+	BASELINE_PROJECT=$(ccm query "is_baseline_project_of('${BASELINE_PROJECT}')" -u -f "%objectname") || BASELINE_PROJECT=""
+	baseline_name=`printf "${BASELINE_PROJECT:-}" | awk -F"~" '{print $1}'`
+	if [[ "${baseline_name:-}" != "" && "${baseline_name}" != "${this_project_name}" ]]; then
+	  printf "Stop traversing - name changed: '${this_project_name}' -> '$baseline_name'\n\n" 1>&2
+	  printf "Get sucessors with '${this_project_name}' of baseline_project '${BASELINE_PROJECT}' (different name) as well\n\n" 1>&2
+	  echo "${this_project4part}" | sed -e 's/ /xxx/g'
+	  ccm query "has_baseline_project('${BASELINE_PROJECT}') and name='${this_project_name}' " -u -f "%objectname" | sed -e 's/ /xxx/g'
+	  exit
 	fi
+	if [[ "${BASELINE_PROJECT:-}" != "" ]] ; then
+		printf "${BASELINE_PROJECT} -> " 1>&2
+	else
+		printf "<void>\n\n" 1>&2
+		break
+  fi
 done
-printf "void\n"
-printf "FINAL $BASELINE_PROJECT"
-exit 0
+printf "${this_project4part}" | sed -e 's/ /xxx/g'
