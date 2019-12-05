a9e67ef3ad865cd0da8a35d9148881a7c997fa15
Claus Schneider  (Praqma) <cls@praqma.net>
2019-12-05 15:30:21 +0100

    retagger correct looping in case of spaces in project version

--- ccm-convert-flat2submodules-existingRepoHistory.sh
+++ ccm-convert-flat2submodules-existingRepoHistory.sh
@@ -134,9 +134,7 @@ function convert_revision(){
         touch .gitmodules && git add ./.gitmodules # make sure we have a clean start for every revision - do not use the .gitmodules as we also need to be able to remove some
     fi
 
-    local repo_subprojects4part=$(ccm query "is_member_of('${ccm_project_4part_spaced}') and name!='${ccm_project_name_spaced}' and type='project'" -u -f "%objectname")
-
-    for repo_submodule4part in ${repo_subprojects4part}; do
+    for repo_submodule4part in $(ccm query "is_member_of('${ccm_project_4part_spaced}') and name!='${ccm_project_name_spaced}' and type='project'" -u -f "%objectname" | sed -e 's/ /xxx/g' ); do
         set +x
         regex_4part='^(.+)~(.+):(.+):(.+)$'
         [[ ${repo_submodule4part} =~ ${regex_4part} ]] || exit 1
