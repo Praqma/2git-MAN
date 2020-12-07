38800a597c0481ee8148466173330abc6428a88b
Claus Schneider(Eficode) <claus.schneider@eficode.com>
2020-12-07 11:22:46 +0100

    handle spaces in project names

--- ccm-get-status-from-baseline-or-project.sh
+++ ccm-get-status-from-baseline-or-project.sh
@@ -2,6 +2,8 @@
 set -u
 set -e
 
+[[ "${debug:-}" == "true" ]] && set -x
+
 # Load functions
 source $(dirname $0)/_ccm-functions.sh || source ./_ccm-functions.sh
 
@@ -13,16 +15,19 @@ repo_convert_instance=$3
 [[ "${repo_convert_rev_tag:-}" == "" ]]   && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
 [[ "${repo_convert_instance:-}" == "x" ]] && ( echo "'repo_convert_rev_tag' not set - exit"   && exit 1 )
 
+ccm_proj_obj_string="${ccm_project_name//xxx/' '}~${repo_convert_rev_tag//xxx/' '}:project:${repo_convert_instance}"
+
+
 exit_code="0"
-find_n_set_baseline_obj_attrs_from_project "${ccm_project_name}~${repo_convert_rev_tag}:project:${repo_convert_instance}" "verbose_false" || exit_code=$?
+find_n_set_baseline_obj_attrs_from_project "${ccm_proj_obj_string}" "verbose_false" || exit_code=$?
 if [[ "${exit_code}" != "0" ]] ; then
-    echo "ERROR: Project not found: ${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}"
+    echo "ERROR: Project not found: ${ccm_proj_obj_string}"
     exit ${exit_code}
 fi
 
 if [[ "${ccm_baseline_status:-}" == "" ]]; then
     # We could not set status from baseline object - take it from the project
-    ccm_baseline_status=`ccm attr -show status "${ccm_project_name}~$(echo ${repo_convert_rev_tag} | sed -e 's/xxx/ /g'):project:${repo_convert_instance}" |  sed -e 's/ //g' |  cut -c1-3`
+    ccm_baseline_status=`ccm attr -show status "${ccm_proj_obj_string}" |  sed -e 's/ //g' |  cut -c1-3`
 else
     ccm_baseline_status=`echo ${ccm_baseline_status} |  cut -c1-3`
 fi
