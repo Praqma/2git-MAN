09445fce271acef8d1ec7919635b1d5aa957763d
Claus Schneider(Eficode) <claus.schneider@eficode.com>
2022-05-09 15:01:24 +0200

    ccm2git: handle spaces in ccm project name in 2git

--- src/main/groovy/toGit/migration/sources/ccm/CCMSource.groovy
+++ src/main/groovy/toGit/migration/sources/ccm/CCMSource.groovy
@@ -30,7 +30,7 @@ class CCMSource implements MigrationSource {
 
         // Build the CCM project conversion list
         def sout = new StringBuilder(), serr = new StringBuilder()
-        def cmd_line = "bash " + System.getProperty("user.dir") + File.separator + "ccm-baseline-history.sh $name4part"
+        def cmd_line = "bash " + System.getProperty("user.dir") + File.separator + "ccm-baseline-history.sh \"name4part\""
         log.info cmd_line.toString()
 
         def envVars = System.getenv().collect { k, v -> "$k=$v" }
